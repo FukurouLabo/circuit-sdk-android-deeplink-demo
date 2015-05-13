f1bf956b9e499bdcb1d5ce1e8f7cd2895bac2cbf
Tomoya Hayashida <joyfulltomoya@gmail.com>
2015-05-13 22:25:34 +0900

    sample image was added

--- app/src/main/java/io/cir/demoapp/DemoAppSQLiteOpenHelper.java
+++ app/src/main/java/io/cir/demoapp/DemoAppSQLiteOpenHelper.java
@@ -15,27 +15,18 @@ public class DemoAppSQLiteOpenHelper extends SQLiteOpenHelper{
             + "IMAGE_ID integer,"
             + "NAME varchar(255),"
             + "PRICE integer,"
-            + "DESCRIPTION text"
-            + " );";
+            + "DESCRIPTION text);";
     private static final String CREATE_TABLE_PURCHASE = "create table PURCHASE ("
             + "ID integer primary key autoincrement,"
             + "ITEM_ID integer,"
             + "QUANTITY integer,"
-            + "PURCHASE_STATE varchar(255)"
-            + " );";
+            + "PURCHASE_STATE varchar(255));";
     private static final String DROP_TABLE_ITEM = "drop table ITEM;";
     private static final String DROP_TABLE_PURCHASE = "drop table PURCHASE;";
     private static final String INITIAL_INSERT_ITEM = "insert into ITEM (IMAGE_ID, NAME, PRICE, DESCRIPTION) values"
-            + "(" + R.drawable.dummy_image + ", '商品1', 1000, 'とてもいい商品です'),"
-            + "(" + R.drawable.dummy_image + ", '商品2', 2000, 'とてもいい商品です'),"
-            + "(" + R.drawable.dummy_image + ", '商品3', 3000, 'とてもいい商品です'),"
-            + "(" + R.drawable.dummy_image + ", '商品4', 4000, 'とてもいい商品です'),"
-            + "(" + R.drawable.dummy_image + ", '商品5', 5000, 'とてもいい商品です'),"
-            + "(" + R.drawable.dummy_image + ", '商品6', 6000, 'とてもいい商品です'),"
-            + "(" + R.drawable.dummy_image + ", '商品7', 7000, 'とてもいい商品です'),"
-            + "(" + R.drawable.dummy_image + ", '商品8', 8000, 'とてもいい商品です'),"
-            + "(" + R.drawable.dummy_image + ", '商品9', 9000, 'とてもいい商品です')"
-            + ";";
+            + "(" + R.drawable.carrot + ", 'ニンジン', 100, '千葉県産。有機農法で作られたニンジンです。ニンジンはカロテン類が豊富であり、半本で1日のビタミンA必要量がとれます。生食、炒める、煮るなど、多くの方法で調理が可能です。'),"
+            + "(" + R.drawable.potato + ", 'ジャガイモ', 50, '北海道産のメークイン。肉じゃがや粉吹き芋、ポテトサラダ、いももち、カレー、シチュー、グラタン、おでん、味噌汁などの具にも広くご利用いただけます。'),"
+            + "(" + R.drawable.tomato + ", 'トマト', 200, '静岡県産。トマトには抗酸化作用を持つとされる成分リコピンが多量に含まれています。甘みが強く、サラダや焼きトマト、サルサ、ピザ、パスタ用ソース、スープなどにおすすめです。');";
 
     public DemoAppSQLiteOpenHelper(Context c) {
         super(c, Config.DB_NAME, null, Config.DB_VERSION);
