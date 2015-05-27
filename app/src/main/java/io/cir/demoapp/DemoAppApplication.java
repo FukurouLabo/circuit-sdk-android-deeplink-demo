ac7673bb856d0dcb3be17b30abdea06e939d2528
Tomoya Hayashida <joyfulltomoya@gmail.com>
2015-05-27 12:31:52 +0900

    developmentMode code added

--- app/src/main/java/io/cir/demoapp/DemoAppApplication.java
+++ app/src/main/java/io/cir/demoapp/DemoAppApplication.java
@@ -2,6 +2,8 @@ package io.cir.demoapp;
 
 import android.app.Application;
 
+import io.cir.DeepLinkSupporterActivity;
+
 /**
  * Created by joytomo on 2015/05/27.
  */
@@ -9,6 +11,10 @@ public class DemoAppApplication extends Application {
 
     @Override
     public void onCreate() {
+        // 開発モードのON/OFF
+        DeepLinkSupporterActivity.setDevelopmentMode(false);
+        // ログ出力のON/OFF
+        DeepLinkSupporterActivity.setLogEnabled(true);
     }
 
 }
